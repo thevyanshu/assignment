cd9dc1375db69444fbd9dc9e7b582505145bdb62
Divyanshu <divyanshujain20010808@gmail.com>
2022-05-12 02:06:54 +0530

    first commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,50 @@
+class UsersController < ApplicationController
+    skip_before_action :authenticate_request, only: [:create]
+    before_action :set_user, only: [:show, :destroy]
+
+
+    #GET /users
+
+    def index
+        @users = User.all
+        render json: @users, status: :ok
+    end
+
+    def show
+        render json: @user, status: :ok
+    end
+
+    # POST /users
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            render json: @user, status: :created
+        else
+            render json: { errors: @user.errors.full.messages }, status: :unprocessable_entity
+        end
+    end
+
+    # PUT /users/{username}
+    def update
+        unless @user.update(user_params)
+            render json: { errors: @user.errors.full.messages }, status: :unprocessable_entity
+        end
+    end
+
+    # DELETE /users/{username}
+    def destroy
+        @user.destroy
+    end
+
+
+    private
+        def user_params
+            params.permit(:username, :password, :email)
+        end
+
+        def set_user
+            @user = User.find(params[:id])
+        end 
+    
+end
